a0d86a3c4ddcae932687fef79e14d76e141f7022
Bill <Bill@TheWallisFamily.co.uk>
2026-06-16 16:58:29 +0100

    feat: add zip command (#3)

--- src/feta/cli.py
+++ src/feta/cli.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import argparse
 import importlib.metadata
 import pathlib
+import re
 from collections.abc import Sequence
 
 import feta.files
@@ -22,9 +23,35 @@ def cmd_read(args: argparse.Namespace) -> int:
             for row in feta.files.read_csv(path):
                 print(row)
         else:
-            print(path.read_text(encoding="utf-8"))
+            print(path.read_text(encoding="utf-8"), end="")
 
     return SUCCESS
+
+
+def cmd_zip(args: argparse.Namespace) -> int:
+    try:
+        feta.files.zip_directory(
+            target=pathlib.Path(args.target),
+            select=args.select,
+            exclude=args.exclude,
+        )
+        return SUCCESS
+    except Exception as e:
+        print(f"error: {e}")
+        return FAILURE
+
+
+def _add_argument__select_exclude(parser: argparse.ArgumentParser) -> None:
+    parser.add_argument(
+        "--select",
+        help="regex pattern to include paths, matches full file path",
+        default="",
+    )
+    parser.add_argument(
+        "--exclude",
+        help="regex pattern to exclude paths, matches full file path",
+        default="^$",
+    )
 
 
 def main(argv: Sequence[str] | None = None) -> int:
@@ -41,12 +68,18 @@ def main(argv: Sequence[str] | None = None) -> int:
     )
     subparsers = parser.add_subparsers(dest="command")
 
-    parser__foo = subparsers.add_parser("read")
-    parser__foo.add_argument("filenames", nargs="+")
+    parser__read = subparsers.add_parser("read")
+    parser__read.add_argument("filenames", nargs="+")
+
+    parser__zip = subparsers.add_parser("zip")
+    parser__zip.add_argument("target")
+    _add_argument__select_exclude(parser__zip)
 
     args = parser.parse_args(argv)
     if args.command == "read":
         return cmd_read(args)
+    if args.command == "zip":
+        return cmd_zip(args)
 
     parser.print_help()
     return SUCCESS
